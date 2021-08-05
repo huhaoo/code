5652c81446b89a9e06c9757ae0a3b16dfd274688
huhaoo <826538400@qq.com>
2021-08-05 09:56:34 +0800

    233

---
+++ codeforces/1336/F.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+#define fr(i,a,b) for(int i=(a),end_##i=(b);i<=end_##i;i++)
+#define fd(i,a,b) for(int i=(a),end_##i=(b);i>=end_##i;i--)
+#define i64 long long
+int read()
+{
+	int r=0,t=1,c=getchar();
+	while(c<'0'||c>'9'){ t=c=='-'?-1:1; c=getchar(); }
+	while(c>='0'&&c<='9'){ r=(r<<3)+(r<<1)+(c^48); c=getchar(); }
+	return r*t;
+}
+const int N=150010;
+int n,q,k;
+int main()
+{
+	return 0;
+}
